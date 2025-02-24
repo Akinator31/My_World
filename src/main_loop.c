195335c67e05030db827d3bf22e97727e1d9f2f2
Pavel <pavel.de-wavrechin@epitech.eu>
2025-02-24 18:01:00 +0100

    fix(main loop): move up poll event for get access to event member of engine struct in scene_update function

--- src/main_loop.c
+++ src/main_loop.c
@@ -26,11 +26,11 @@ int main(int ac, char **av, char **envp)
     while ((engine != NULL) && sfRenderWindow_isOpen(engine->window)) {
         sfRenderWindow_clear(engine->window, sfBlack);
         load_current_scene(engine);
+        while (sfRenderWindow_pollEvent(engine->window, &engine->event))
+            analyse_event(engine->window, &engine->event);
         if (engine->state == RUNNING && ((scene_t *)(engine->current_scene))
             ->scene_update(engine->current_scene, engine) == 84)
             return engine_destroy(engine);
-        while (sfRenderWindow_pollEvent(engine->window, &engine->event))
-            analyse_event(engine->window, &engine->event);
         sfRenderWindow_display(engine->window);
     }
     return engine_destroy(engine);
