84b6d1cd35e0ca73c37d513df6d061376d00d917
Pavel <pavel.de-wavrechin@epitech.eu>
2025-03-07 17:37:27 +0100

    feat(map): add texture on map tiles and change effect when editing the terrain

--- src/utils/draw_2d_map.c
+++ src/utils/draw_2d_map.c
@@ -6,6 +6,7 @@
 */
 
 #include <SFML/Window.h>
+#include <SFML/System.h>
 #include <SFML/Config.h>
 #include <SFML/Graphics.h>
 #include <SFML/Audio.h>
@@ -14,12 +15,13 @@
 #include <math.h>
 #include "structure.h"
 #include "utils.h"
+#include "ressources.h"
 
 sfVertexArray *create_line(sfVertexArray *vertex_array, sfVector2f *point1,
     sfVector2f *point2)
 {
-    sfVertex vertex1 = {.position = *point1, .color = sfWhite};
-    sfVertex vertex2 = {.position = *point2, .color = sfWhite};
+    sfVertex vertex1 = {.position = *point1, .color = sfBlack};
+    sfVertex vertex2 = {.position = *point2, .color = sfBlack};
 
     sfVertexArray_append(vertex_array, vertex1);
     sfVertexArray_append(vertex_array, vertex2);
@@ -30,19 +32,20 @@ sfVertexArray *create_line(sfVertexArray *vertex_array, sfVector2f *point1,
 static int draw_vertex(engine_t *engine, int i, int j)
 {
     map_t *map = engine->map;
+    sfRenderStates state = get_state(engine);
 
     if (i + 1 < map->size_tab) {
         map->vertex_array = sfVertexArray_create();
         sfRenderWindow_drawVertexArray(engine->window,
             create_line(map->vertex_array, &map->map2D[i][j],
-                &map->map2D[i + 1][j]), NULL);
+                &map->map2D[i + 1][j]), &state);
         sfVertexArray_destroy(map->vertex_array);
     }
     if (j + 1 < map->size_tab) {
         map->vertex_array = sfVertexArray_create();
         sfRenderWindow_drawVertexArray(engine->window,
             create_line(map->vertex_array, &map->map2D[i][j],
-                &map->map2D[i][j + 1]), NULL);
+                &map->map2D[i][j + 1]), &state);
         sfVertexArray_destroy(map->vertex_array);
     }
     return 0;
@@ -50,19 +53,19 @@ static int draw_vertex(engine_t *engine, int i, int j)
 
 static sfVertexArray *create_quads(map_t *map, int i, int j)
 {
-    sfVertex vertex1 = {.position = map->map2D[i][j], .color = sfBlack};
-    sfVertex vertex2 = {.position = map->map2D[i + 1][j], .color = sfBlack};
-    sfVertex vertex4 = {.position = map->map2D[i][j + 1], .color = sfBlack};
-    sfVertex vertex3 = {.position = map->map2D[i + 1][j + 1],
-        .color = sfWhite};
+    sfVertex vertex1 = {map->map2D[i][j], sfBlack, (sfVector2f){0, 0}};
+    sfVertex vertex2 = {map->map2D[i + 1][j], sfWhite, (sfVector2f){154, 0}};
+    sfVertex vertex4 = {map->map2D[i][j + 1], sfWhite, (sfVector2f){154, 151}};
+    sfVertex vertex3 = {map->map2D[i + 1][j + 1],
+        sfBlack, (sfVector2f){0, 151}};
 
     if (map->map3D[i][j] == map->map3D[i + 1][j]
         && map->map3D[i][j + 1] == map->map3D[i + 1][j + 1]
         && map->map3D[i][j] == map->map3D[i][j + 1]) {
-    vertex1.color = sfColor_fromRGB(156, 156, 156);
-    vertex2.color = sfColor_fromRGB(156, 156, 156);
-    vertex3.color = sfColor_fromRGB(156, 156, 156);
-    vertex4.color = sfColor_fromRGB(156, 156, 156);
+            vertex1.color = sfWhite;
+            vertex2.color = sfWhite;
+            vertex3.color = sfWhite;
+            vertex4.color = sfWhite;
         }
     sfVertexArray_append(map->vertex_array, vertex1);
     sfVertexArray_append(map->vertex_array, vertex2);
@@ -75,11 +78,12 @@ static sfVertexArray *create_quads(map_t *map, int i, int j)
 static void draw_square(engine_t *engine, int i, int j)
 {
     map_t *map = engine->map;
+    sfRenderStates state = get_state(engine);
 
     if (i + 1 < map->size_tab && j + 1 < map->size_tab) {
         map->vertex_array = sfVertexArray_create();
         sfRenderWindow_drawVertexArray(engine->window,
-            create_quads(map, i, j), NULL);
+            create_quads(map, i, j), &state);
         sfVertexArray_destroy(map->vertex_array);
     }
 }
